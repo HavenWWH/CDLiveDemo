32ba768af0c84552945bd75e8f965004bb38529f
cqz <cqz@ttsing.com>
2019-03-10 21:11:37 +0800

    喜满体好有喜感

--- CoursewareDownload/CoursewareDownload/ViewController.m
+++ CoursewareDownload/CoursewareDownload/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UILabel *titleLabel;
+
 @end
 
 @implementation ViewController
@@ -19,6 +21,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+
+    [self.view addSubview:self.titleLabel];
 }
 
 #pragma mark - Intial Methods
@@ -33,5 +38,15 @@
 #pragma mark - External Delegate
 
 #pragma mark - Setter Getter Methods
+- (UILabel *)titleLabel {
+    if (!_titleLabel) {
+        _titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(15, 100, [UIScreen mainScreen].bounds.size.width - 30, 50)];
+        _titleLabel.font = [UIFont fontWithName:@"XIMANJW" size:20];
+        _titleLabel.textAlignment = NSTextAlignmentCenter;
+        _titleLabel.textColor = [UIColor blackColor];
+        _titleLabel.text = @"喜满体的标题好有喜感";
+    }
+    return _titleLabel;
+}
 
 @end
